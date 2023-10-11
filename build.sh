81de46848419e8a3002d0b31e8afd229e08b120f
decimatedTomato <greentomatopower@gmail.com>
2023-10-11 16:58:34 +0200

    Removed running line from build script

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ set -xe
 mkdir -p bin
 cp lib/glew32.dll bin/glew32.dll
 gcc src/simulation.c -o bin/simulation -Iinclude -Llib -lmingw32 -lglfw3 -lopengl32 -lgdi32 -luser32 -lglew32 -Wall -Wextra
-
-./bin/simulation
