174991d484edb9893a77194510d684157b80ad04
decimatedTomato <greentomatopower@gmail.com>
2023-10-11 17:02:03 +0200

    Added a build script with debugging capability

---
+++ build_gdb.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -xe
+
+mkdir -p bin
+cp lib/glew32.dll bin/glew32.dll
+gcc src/simulation.c -g -o bin/simulation -Iinclude -Llib -lmingw32 -lglfw3 -lopengl32 -lgdi32 -luser32 -lglew32 -Wall -Wextra
